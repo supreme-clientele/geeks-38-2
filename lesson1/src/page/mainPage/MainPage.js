f9d21162fd6000bc263af453bfd077f3d63a3f9b
Murat Bukarbaev <murat.bukarbaev@gmail.com>
2024-07-05 12:14:37 +0600

    lesson1 homework

--- lesson1/src/page/mainPage/MainPage.js
+++ lesson1/src/page/mainPage/MainPage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import User from "../../components/user/User";
+import Schools from "../../components/schools/Schools";
 
 const MainPage = () => {
     return (
@@ -10,6 +11,9 @@ const MainPage = () => {
             <Header/>
             <h1>MainPage</h1>
             <User name={'Murat'} age={37} email={'murat.bukarbaev@gmail.com'}/>
+
+            <Schools schoolName={'Geeks'} description={'Международная IT-академия'} address={'БЦ Victory, улица Ибраимова, 103, Бишкек'}/>
+
             <Footer/>
         </div>
     );
